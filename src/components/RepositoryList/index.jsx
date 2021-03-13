ffb3c00ba3cdc62ab09e248ab76b590f15a67ead
Alisson Kohatsu <kohatsukendi@gmail.com>
2021-03-13 11:07:32 -0300

    Created component RepositoryItem and include react refresh into project

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,29 +1,21 @@
+import RepositoryItem from "../RepositoryItem"
+
+const repository = {
+  name: 'Unform',
+  description: 'Unform in React',
+  link: 'https://github.com/unform/unform'
+}
+
 export default function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="">Acessar repopsitório</a>
-        </li>
-
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="">Acessar repopsitório</a>
-        </li>
-
-        <li>
-          <strong>Unform</strong>
-          <p>Forms in ReactJS</p>
-
-          <a href="">Acessar repopsitório</a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   )
